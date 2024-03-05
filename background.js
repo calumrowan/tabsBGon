b4c5f537462208b0a44baa93b1c5bf6f99c23bd6
calumrowan <63161217+calumrowan@users.noreply.github.com>
2024-03-05 18:22:25 +0000

    Various fixes / updated
    Now fully functional - deletion now restricted to active window. Update requires commands permission for hotkeys

--- background.js
+++ background.js
@@ -1,18 +1,24 @@
-function announce_close(){
-	console.log("closing all matching tabs")
-	chrome.tabs.query({}, function(tabs) {
-		var closeable=[];
-		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-	    let url = tabs[0].url;
-		});
-		var domain = url.substring(0, url.indexOf("/")).length
-		for (var i=0; i<tabs.length; ++i) {
-			console.log(tabs[i]);
-			if(tabs[i].url.includes(domain)) {	
-				closeable.push(tabs[i].id)
-			}
-		}
-		chrome.tabs.remove(closeable);
-	});
+function announce_close() {
+    console.log("closing all matching tabs");
+    chrome.tabs.query({currentWindow: true }, function (tabs) {
+        var closeable = [];
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (activeTabs) {
+            let urlObject = new URL(activeTabs[0].url);
+            let domain = urlObject.hostname;
+            console.log(domain);
+
+            for (var i = 0; i < tabs.length; ++i) {
+                console.log(tabs[i]);
+                if (new URL(tabs[i].url).hostname === domain) {
+                    closeable.push(tabs[i].id);
+                }
+            }
+
+            chrome.tabs.remove(closeable);
+        });
+    });
 }
-chrome.browserAction.onClicked.addListener(function(tab){announce_close()});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    announce_close();
+});
