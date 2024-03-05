ec58d3abb4a578c93d1b07cf31c9800b793ede60
calumrowan <63161217+calumrowan@users.noreply.github.com>
2024-03-05 17:30:14 +0000

    Initial changes test

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 function announce_close(){
-	console.log("closing all search tabs")
+	console.log("closing all matching tabs")
 	chrome.tabs.query({}, function(tabs) {
 		var closeable=[];
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+	    let url = tabs[0].url;
+		});
+		var domain = url.substring(0, url.indexOf("/")).length
 		for (var i=0; i<tabs.length; ++i) {
 			console.log(tabs[i]);
-			if(tabs[i].url.includes("google.com/search?")) {	
-				closeable.push(tabs[i].id)
-			} else if(tabs[i].url.includes("duckduckgo.com/?q=")){
+			if(tabs[i].url.includes(domain)) {	
 				closeable.push(tabs[i].id)
 			}
 		}
